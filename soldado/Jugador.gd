834f887c8ed985f6ad582de6ef50b238b1ec82cc
Omar Corpus <ocorpus@ucol.mx>
2020-02-01 19:10:13 -0600

    animaciones de idle, segun direc del sprite

--- soldado/Jugador.gd
+++ soldado/Jugador.gd
@@ -18,25 +18,53 @@ func control(delta):
 func movement_loop():
 	var motion = movedir.normalized() * speed 
 	move_and_slide(motion, Vector2(0,0))
-	
+
+#variabe para guardar direccion de ultima direccion
+var last_dir
 func animation_loop():
 	match movedir:
 		Vector2(-1,0): #left
 			$Sprite.play("run_left")
+			last_dir = "left"
 		Vector2(1,0): #right
 			$Sprite.play("run_right")
+			last_dir = "right"
 		Vector2(0,-1): #up
 			$Sprite.play("run_up")
+			last_dir = "up"
 		Vector2(0,1): #down
 			$Sprite.play("run_down")
+			last_dir = "down"
 		Vector2(-1,-1): #up left
-			$Sprite.play("up_left")
+			$Sprite.play("run_up_left")
+			last_dir = "up_left"
 		Vector2(-1,1): #down left
-			$Sprite.play("down_left")
+			$Sprite.play("run_left_down")
+			last_dir = "down_left"
 		Vector2(1,-1): #right up
-			$Sprite.play("up_right")
+			$Sprite.play("run_up_right")
+			last_dir = "up_right"
 		Vector2(1,1): #right down
-			$Sprite.play("down_right")
-		Vector2(0,0): #iddle
-			$Sprite.play("idle")
+			$Sprite.play("run_right_down")
+			last_dir = "down_right"
+		
+		Vector2(0,0): #iddle switch
+			match last_dir:
+				"left":
+					$Sprite.play("idle_left")
+				"right":
+					$Sprite.play("idle_right")
+				"up":
+					$Sprite.play("idle_up")
+				"down":
+					$Sprite.play("idle_down")
+				"up_left":
+					$Sprite.play("idle_left_up")
+				"down_left":
+					$Sprite.play("idle_down_left")
+				"up_right":
+					$Sprite.play("idle_right_up")
+				"down_right":
+					$Sprite.play("idle_down_right")		
+		
 		
